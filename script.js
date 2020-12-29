531465f4a4d1afb5ddbd6bb39eef6fd8fe518d96
hazedrops <hazedrops@hotmail.com>
2020-12-28 16:39:19 -0800

    Simple sound play project using the <audio> tag

---
+++ script.js
@@ -0,0 +1,27 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach(sound => {
+  const btn = document.createElement('button')
+  btn.classList.add('btn')
+
+  btn.innerText = sound
+
+  btn.addEventListener('click', () => {
+    stopSounds()
+
+    document.getElementById(sound).play()
+  })
+  
+  document.getElementById('buttons').
+    appendChild(btn)
+})
+
+function stopSounds() {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound)
+
+    // stopping a song
+    song.pause()
+    song.currentTime = 0
+  })
+}
